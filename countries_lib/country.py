d528e31870ba60e624ff4daf92bd8328f275def3
Aleksey <aleksey.pauls@mail.ru>
2017-07-24 10:56:38 +0700

    Little update - add non-space logic and database without special symbols

--- countries_lib/country.py
+++ countries_lib/country.py
@@ -22,7 +22,7 @@ def match_country_name(key, value, priority=2):
         except Exception:
             return 'DatabaseError'
     else:
-        return 'Invalid argument type'
+        return 'Invalid arguments'
 
 
 def del_country_name(key):
@@ -38,7 +38,7 @@ def del_country_name(key):
         except Exception:
             return 'DatabaseError'
     else:
-        return 'Invalid argument type'
+        return 'Invalid arguments'
 
 
 def normalize_country_name(posname, dif_acc=0.7):
@@ -47,11 +47,11 @@ def normalize_country_name(posname, dif_acc=0.7):
     Возвращаемое значение: общее название страны (строка), если не найдено - 'None' (строка) """
 
     if type(posname) is not str or type(dif_acc) is not float or dif_acc <= 0.0 or dif_acc >= 1.0:
-        return 'Invalid argument type'
+        return 'Invalid arguments'
     try:
         with shelve.open(DB_PATH, 'w') as countries_db:
-            posname = str(posname).lower()
-            # Очищаем входную строку от знаков препинания, которые не встречаются в названиях стран
+            posname = posname.lower()
+            # Очищаем входную строку от знаков препинания (кроме пробелов), которые не встречаются в названиях стран
             # Данный способ безопаснее, чем регулярные выражения и применение некоторых стандартных функций,
             # всвязи с национальными символами в названиях стран. Если пользователь умудрится использовать
             # иные символы, то он либо опечатался, либо издевается. Опечатки исправляются далее.
@@ -71,6 +71,19 @@ def normalize_country_name(posname, dif_acc=0.7):
             if posname_2 != [] and countries_db[posname_2[0]][0] == '2' and \
                     len(posname) - len(posname_2[0]) <= 1:
                 return countries_db[posname_2[0]][1:]
+            # Ищем совпадение всей строки без пробелов и значения с приоритетом '1'
+            posname_3 = posname.replace(' ', '')
+            posname_3 = difflib.get_close_matches(posname_3, countries_db.keys(), n=1, cutoff=dif_acc)
+            if posname_3 != [] and countries_db[posname_3[0]][0] == '1' and \
+                    len(posname.replace(' ', '')) - len(posname_3[0]) <= 1:
+                return countries_db[posname_3[0]][1:]
+            # Ищем совпадение всей строки без пробелов и значения с приоритетом '2'
+            posname_4 = posname.replace(' ', '')
+            posname_4 = difflib.get_close_matches(posname_4, countries_db.keys(), n=1, cutoff=dif_acc)
+            if posname_4 != [] and countries_db[posname_4[0]][0] == '2' and \
+                    len(posname.replace(' ', '')) - len(posname_4[0]) <= 1:
+                return countries_db[posname_4[0]][1:]
+
             # Делим входную строку на слова, разделитель - пробел
             parts = posname.split(" ")
             for part in parts:
@@ -98,3 +111,6 @@ def normalize_country_name(posname, dif_acc=0.7):
     # что ошибка здесь может быть только в отсутствии корректной базы данных
     except Exception:
             return 'DatabaseError'
+
+if __name__ == '__main__':
+    print(normalize_country_name(input()))
